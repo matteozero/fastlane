5b6d6a428fa088907accda75d4d053caa2ef0676
Matteo <immatteoz@gmail.com>
2024-09-11 14:39:03 +0800

    xcframework

--- fastlane/actions/yk_build_framework.rb
+++ fastlane/actions/yk_build_framework.rb
@@ -3,76 +3,43 @@ module Fastlane
     class YkBuildFrameworkAction < Action
       def self.run(params)
         build_target = params[:project]
-
         current_path = Dir.pwd
         # 删除当前目录旧的framework和dsym文件
-        framework = "#{current_path}/fmk/#{build_target}.framework"
+        framework = "#{current_path}/fmk/#{build_target}.xcframework"
         if File.exist?(framework)
           FileUtils.rm_rf(framework)
         end
-        dsym = "#{current_path}/fmk/#{build_target}.framework.dSYM"
+        dsym = "#{current_path}/fmk/#{build_target}.xcframework.dSYM"
         if File.exist?(dsym)
           FileUtils.rm_rf(dsym)
         end
 
         build_path = "#{current_path}/build"
+        Actions.sh("rm -rf #{build_path}/")
+
         build_project = "#{current_path}/Example/Pods/Pods.xcodeproj"
 
         # 编译configuration，默认为Release
         build_config = "Release"
+        
+        clean_command = "xcodebuild clean -project #{build_project} -scheme #{build_target} -configuration #{build_config}"
+        Actions.sh(clean_command)
 
-        # 使用archive构建真机framework
-        archive_command = "xcodebuild clean archive -project #{build_project} -scheme #{build_target} -configuration #{build_config} -sdk iphoneos OBJROOT=#{build_path} SYMROOT=#{build_path} ONLY_ACTIVE_ARCH=NO | xcpretty"
-        Actions.sh(archive_command)
+        # 构建真机xcarchive
+        iphoneos_command = "xcodebuild archive -project #{build_project} -scheme #{build_target} -configuration Release -destination 'generic/platform=iOS' -archivePath ./build/#{build_target}-iphoneos.xcarchive SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES"
+        Actions.sh(iphoneos_command)
 
-        # 将真机framework复制到项目根目录
-        iphone_framework="#{build_path}/UninstalledProducts/iphoneos/#{build_target}.framework"
-        target_framework="#{current_path}/fmk/#{build_target}.framework"
+        # 构建模拟器xcarchive
+        simulator_command = "xcodebuild archive -project #{build_project} -scheme #{build_target} -configuration Release -destination 'generic/platform=iOS Simulator' -archivePath ./build/#{build_target}-iphonesimulator.xcarchive SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES"
+        Actions.sh(simulator_command)
 
-        copy_command = "cp -R #{iphone_framework} #{current_path}/fmk/"
+        # 构建xcframework
+        xcframework_command = "xcodebuild -create-xcframework -framework ./build/#{build_target}-iphoneos.xcarchive/Products/Library/Frameworks/#{build_target}.framework -framework ./build/#{build_target}-iphonesimulator.xcarchive/Products/Library/Frameworks/#{build_target}.framework -output ./build/#{build_target}.xcframework"
+        Actions.sh(xcframework_command)
+
+        copy_command = "cp -R ./build/#{build_target}.xcframework #{current_path}/fmk/"
         Actions.sh(copy_command)
 
-        # copy真机符号表文件
-        dsym_path = "#{build_path}/#{build_config}-iphoneos/#{build_target}/#{build_target}.framework.dSYM"
-        if File.directory?(dsym_path)
-          copy_dsym_command = "cp -R #{dsym_path} #{current_path}/fmk/"
-          Actions.sh(copy_dsym_command)
-        end
-
-        # 使用build构建模拟器framework
-        build_command = "xcodebuild clean build -project #{build_project} -scheme #{build_target} -configuration #{build_config} -sdk iphonesimulator OBJROOT=#{build_path} SYMROOT=#{build_path} ONLY_ACTIVE_ARCH=NO | xcpretty"
-        Actions.sh(build_command)
-
-        simulator_framework="#{build_path}/#{build_config}-iphonesimulator/#{build_target}/#{build_target}.framework"
-
-        # 将模拟器的swift modules（如果存在）复制到fmk的framework目录
-        simulator_swift_module = "#{simulator_framework}/Modules/#{build_target}.swiftmodule/."
-        if File.exist?(simulator_swift_module)
-          copy_swift_module_command = "cp -R #{simulator_swift_module} #{target_framework}/Modules/#{build_target}.swiftmodule"
-          Actions.sh(copy_swift_module_command)
-
-
-          #merge swift pod header file
-          rename_iphoneos_header_file_command = "mv #{target_framework}/Headers/#{build_target}-Swift.h #{target_framework}/Headers/#{build_target}-Swift-iphoneos.h"
-          Action.sh(rename_iphoneos_header_file_command)
-          rename_simulator_header_file_command = "cp #{simulator_framework}/Headers/#{build_target}-Swift.h #{target_framework}/Headers/#{build_target}-Swift-iphonesimulator.h"
-          Action.sh(rename_simulator_header_file_command)
-
-          merged_header_file_name = "#{target_framework}/Headers/#{build_target}-Swift.h"
-          Action.sh("echo '#if __x86_64__ || __i386__ ' > #{merged_header_file_name}")
-          Action.sh("echo '#include \"#{build_target}-Swift-iphonesimulator.h\"' >> #{merged_header_file_name}")
-          Action.sh("echo '#else' >> #{merged_header_file_name}")
-          Action.sh("echo '#include \"#{build_target}-Swift-iphoneos.h\"' >> #{merged_header_file_name}")
-          Action.sh("echo '#endif' >> #{merged_header_file_name}")
-
-        end
-
-        # 合并真机和模拟器二进制文件
-        lipo_command = "lipo -create -output #{target_framework}/#{build_target} #{simulator_framework}/#{build_target} #{target_framework}/#{build_target}"
-        Actions.sh(lipo_command)
-
-        # 清理build文件
-        Actions.sh "rm -rf #{build_path}"
       end
 
       #####################################################
